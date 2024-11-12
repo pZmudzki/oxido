97e9b5c32ffad182de2dc596af9af82e9d668b77
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-11-13 00:53:04 +0100

    Implement function for fetching generated html ,based on passed article content, from openai api.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+require("dotenv").config();
 const fs = require("fs");
+const OpenAI = require("openai");
+
+// openAi instance
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 /**
  * Function to load article from file
@@ -10,14 +17,38 @@ const loadArticle = (filePath) => {
 };
 
 /**
+ * Function to generate HTML from article content using OpenAI
+ * @param {string} articleContent
+ * @returns {Promise<string>} generated HTML
+ * @async
+ */
+const generateHTML = async (articleContent) => {
+  const prompt = ` Generate HTML for this article with appropriate HTML tags. Include <img src="image_placeholder.jpg" alt="prompt to generate image" /> tags where images should go. Do not add any css or javascript. Return only article content without any <html>, <head> and <body> tags. Do not modify the article content. Do not add any text before nor after generated HTML.`;
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "user",
+        content: prompt + " Acticle: " + articleContent,
+      },
+    ],
+  });
+
+  return response.choices[0].message.content;
+};
+
+/**
  * Main self invoked function
  * @returns {void}
  */
-(function () {
+(async function () {
   try {
     const articlePath = "./article.txt";
     const articleContent = loadArticle(articlePath);
-    console.log(articleContent);
+
+    const htmlContent = await generateHTML(articleContent);
+    console.log(htmlContent);
   } catch (error) {
     console.error("Błąd:", error);
   }
